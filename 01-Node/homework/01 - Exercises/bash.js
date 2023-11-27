e7e9dbf83da00bccf058cac285185e74607ee0e1
erikayeah <erikafogar.j@gmail.com>
2023-11-27 20:57:38 +0100

    node

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -1,8 +1,47 @@
 const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
+const { log } = require('console');
 
-function bash() {}
+
+const bash = () => {
+   process.stdout.write("prompt > ");
+   process.stdin.on('data', (data) => {
+   //* Aquí puedes manejar la entrada de datos (comandos) del usuario
+   //* La variable 'data' contendrá la entrada del usuario
+   const args = data.toString().trim().split(' '); //*Lo convierto a string y le saco los espacios de mas por delante, y divide la cadena en cada espacio en blanco.
+   const cmd = args[0];
+
+   if (commands.hasOwnProperty(cmd)) {
+
+      commands[cmd](print, args);
+
+   } else {
+      print(`command not found: ${cmd}`)
+   }
+   })
+}
+
+
+// const verificar = () => {
+
+//    if (commands.hasOwnProperty('cmd')) {
+//       // Si existe, ejecuta la función correspondiente al comando
+//       console.log('SI EXISTE!!!!');;
+//     } else {
+//       console.log(`NO ESTAAAAAAAAAAA`);
+//     }
+//    }
+
+ //  console.log(verificar());
+
+const print = (output) => {
+//* stdout: Es una abreviatura de "standard output" (salida estándar) y es un flujo de datos que se utiliza para enviar información desde un programa de computadora a la pantalla o a otro programa.
+//*.write(): Es un método que se utiliza para escribir datos en un flujo de salida. En este caso, se está escribiendo el contenido de la variable output en la salida estándar.
+process.stdout.write(output);
+process.stdout.write("\nprompt > ");
+};
+
 
 bash();
 module.exports = {
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -2,20 +2,80 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+   print(process.cwd());
+};
 
-function date() {}
+function date(print) {
+   print(Date());
+}
 
-function echo() {}
+function echo(print, ...args) {
+print(...args)
+}
 
-function ls() {}
+function ls(print) {
+   fs.readdir('.', (error, files) => {
+      if (error) {
+         throw error
+      } else {
 
-function cat() {}
+      const filesString = files.join(' ');
+      print(filesString);
+      }
+   })
+}
 
-function head() {}
+function cat(print, ...args) {
+   fs.readFile(...args, 'utf-8', (error, data) => {
+      if (error) {
+         throw(error)
+      } else {
+         print (data)
+      }
+   })
+}
 
-function tail() {}
+function head(print, ...args) {
+   fs.readFile(...args, 'utf-8', (error, data) => {
+      if (error) {
+         throw(error)
+      } else {
+         const primeraLinea = data.split('\n')[0];
+         print(primeraLinea);
+      }
+   })
+}
 
-function curl() {}
+function tail(print, ...args) {
+   fs.readFile(...args, 'utf-8', (error, data) => {
+      if (error) {
+         throw(error)
+      } else {
+         const linea = data.split('\n');
+         const ultimaLinea = linea[linea.length - 1].trim();
+         print(ultimaLinea);
+      }
+   })
+}
 
-module.exports = {};
+function curl(print, ...args) {
+   utils.request(...args, (error, response) => {
+      if (error) {
+         throw(error)
+      } else{
+         print(response)
+      }
+   })
+}
+
+module.exports = {
+   pwd,
+   date,
+   echo,
+   ls,
+   cat,
+   head,
+   tail,
+   curl
+};
