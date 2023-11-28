20e89b6f02e7bf9c60544c2e774a5b947d4a3c28
erikayeah <erikafogar.j@gmail.com>
2023-11-28 14:21:33 +0100

    Node

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -4,23 +4,39 @@ const commands = require('./commands/index.js');
 const { log } = require('console');
 
 
+const print = (output) => {
+//* stdout: Es una abreviatura de "standard output" (salida estándar) y es un flujo de datos que se utiliza para enviar información desde un programa de computadora a la pantalla o a otro programa.
+//*.write(): Es un método que se utiliza para escribir datos en un flujo de salida. En este caso, se está escribiendo el contenido de la variable output en la salida estándar.
+process.stdout.write(output);
+process.stdout.write("\nprompt > "); // \n es un salto de linea.
+};
+
+
+
 const bash = () => {
    process.stdout.write("prompt > ");
-   process.stdin.on('data', (data) => {
+   process.stdin.on('data', (data) => {//Me permite captutar info de la terminal.
    //* Aquí puedes manejar la entrada de datos (comandos) del usuario
    //* La variable 'data' contendrá la entrada del usuario
-   const args = data.toString().trim().split(' '); //*Lo convierto a string y le saco los espacios de mas por delante, y divide la cadena en cada espacio en blanco.
-   const cmd = args[0];
+   let args = data.toString().trim().split(' '); //*Lo convierto a string y le saco los espacios de mas por delante, y divide la cadena en cada espacio en blanco. Al final lo paso a array con split para poder sacar posicion 0.
+   const cmd = args.shift(); //saco primer elemento y lo guardo en cmd
+   args = args.join(' '); //lo vuelvo a string para que le llegue como string a Echo, que es lo que esta esperando.
 
-   if (commands.hasOwnProperty(cmd)) {
-
-      commands[cmd](print, args);
-
-   } else {
-      print(`command not found: ${cmd}`)
-   }
+   //*Con ternario
+   commands[cmd]
+      ? commands[cmd](print, args)
+      : print(`command not found: ${cmd}`);
    })
 }
+
+   //* Con if
+   // if (commands.hasOwnProperty(cmd)) { // commands[cmd] otra opcion.
+   //    commands[cmd](print, args); //commands[cmd] es una funcion por eso la ejecuto.
+   // } else {
+   //    print(`command not found: ${cmd}`)
+   // }
+   // })}
+
 
 
 // const verificar = () => {
@@ -32,15 +48,8 @@ const bash = () => {
 //       console.log(`NO ESTAAAAAAAAAAA`);
 //     }
 //    }
-
  //  console.log(verificar());
 
-const print = (output) => {
-//* stdout: Es una abreviatura de "standard output" (salida estándar) y es un flujo de datos que se utiliza para enviar información desde un programa de computadora a la pantalla o a otro programa.
-//*.write(): Es un método que se utiliza para escribir datos en un flujo de salida. En este caso, se está escribiendo el contenido de la variable output en la salida estándar.
-process.stdout.write(output);
-process.stdout.write("\nprompt > ");
-};
 
 
 bash();
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd(print) {
+function pwd(print, args) {
    print(process.cwd());
 };
 
@@ -10,24 +10,23 @@ function date(print) {
    print(Date());
 }
 
-function echo(print, ...args) {
-print(...args)
+function echo(print, args) { //Aca podria ahcer tmb el join de args, aca o en bash, pero lo hago en bash asi funciona para todos los comandos.
+print(args)
 }
 
 function ls(print) {
    fs.readdir('.', (error, files) => {
+      
       if (error) {
          throw error
       } else {
-
-      const filesString = files.join(' ');
-      print(filesString);
+      print(files.join(' '));
       }
    })
 }
 
-function cat(print, ...args) {
-   fs.readFile(...args, 'utf-8', (error, data) => {
+function cat(print, args) {
+   fs.readFile(args, 'utf-8', (error, data) => {
       if (error) {
          throw(error)
       } else {
@@ -36,36 +35,33 @@ function cat(print, ...args) {
    })
 }
 
-function head(print, ...args) {
-   fs.readFile(...args, 'utf-8', (error, data) => {
+function head(print, args) {
+   fs.readFile(args, 'utf-8', (error, data) => {
       if (error) {
          throw(error)
       } else {
-         const primeraLinea = data.split('\n')[0];
+         const primeraLinea = data.split('\n')[0];//Separa por el salto de linea /n
          print(primeraLinea);
       }
    })
 }
 
-function tail(print, ...args) {
-   fs.readFile(...args, 'utf-8', (error, data) => {
+function tail(print, args) {
+   fs.readFile(args, 'utf-8', (error, data) => {
       if (error) {
          throw(error)
       } else {
          const linea = data.split('\n');
-         const ultimaLinea = linea[linea.length - 1].trim();
+         const ultimaLinea = linea.at(-1).trim(); 
          print(ultimaLinea);
       }
    })
 }
 
-function curl(print, ...args) {
-   utils.request(...args, (error, response) => {
-      if (error) {
-         throw(error)
-      } else{
-         print(response)
-      }
+function curl(print, args) {
+   utils.request(args, (error, response) => {
+      if (error)  throw new Error (error); //Asi, al instanciar nuevo error, corto la ejecucion, asi q abajo sigo normalmente. Esto puedo hacerlo tmb en las otras fn.
+      print(response) 
    })
 }
 
